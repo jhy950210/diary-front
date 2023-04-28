7493376bbffe4dfc5cb92f3a1f87f169acf98013
정휘영 <66720964+jhy950210@users.noreply.github.com>
2023-04-28 11:57:18 +0900

    Add form > alert

--- pages/workout/create/index.tsx
+++ pages/workout/create/index.tsx
@@ -1,14 +1,47 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { FormEvent } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function MyForm() {
+  function handleSubmit(e: { preventDefault: () => void; target: any; }) {
+    // Prevent the browser from reloading the page
+    e.preventDefault();
+
+    // Read the form data
+    const form = e.target;
+    const formData = new FormData(form);
+
+    const formJson = Object.fromEntries(formData.entries());
+    
+    let message = "";
+    for(const value in formJson){
+      message += value + ": " + formJson[value] + "\n";
+    }
+    alert(message);
+  }
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      
-    </main>
-  )
+    <form method="post" onSubmit={handleSubmit}>
+      <label>
+        Text input: <input name="myInput" defaultValue="Some initial value" />
+      </label>
+      <hr />
+      <label>
+        Checkbox: <input type="checkbox" name="myCheckbox" defaultChecked={true} />
+      </label>
+      <hr />
+      <p>
+        Radio buttons:
+        <label><input type="radio" name="myRadio" value="option1" /> Option 1</label>
+        <label><input type="radio" name="myRadio" value="option2" defaultChecked={true} /> Option 2</label>
+        <label><input type="radio" name="myRadio" value="option3" /> Option 3</label>
+      </p>
+      <hr />
+      <button type="reset">Reset form</button>
+      <button type="submit">Submit form</button>
+    </form>
+  );
+  
 }
